3ad2bc9c1bae61c9f04e80cb12fa90d480b6af3c
elliott-ribner <elr886@gmail.com>
2017-02-28 14:38:54 -0500

    working on one off flow numbers

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+"use strict";
 const MongoClient = require('mongodb').MongoClient;
 const _ = require('lodash');
 const fs = require('fs');
@@ -6,12 +7,51 @@ const csv = require('fast-csv');
 const conf = require('./conf');
 let _db;
 let _aiMatcherDb;
-ObjectId = require('mongodb').ObjectID;
+let ObjectId = require('mongodb').ObjectID;
 let Users;
 let BrandedUserProfiles;
 let SmsLogs;
 let BrandedApplicantConversations;
 let MatcherLogs;
+let keyLog = {}
+let keyList = { pounceIntroBot: 'false',
+  pounceIntentBot: 'false',
+  campusIdBot: 'false',
+  parkingBot: 'false',
+  intentFollowUpBot: 'false',
+  tuitionFeesPaidBot: 'false',
+  mealPlanNudgeBot: 'false',
+  aidOrientationDayOneCaseTwoBot: 'false',
+  studentBelongingBot: 'false',
+  finalStudySurveyBot: 'false',
+  finalSenseBelongingBot: 'false',
+  acceptingLoansOneBot: 'false',
+  finalStudySurveyAltBot: 'false',
+  acceptFinAidBot: 'false',
+  scholarshipBot: 'false',
+  pounceHousingBot: 'false',
+  senseOfBelongingFollowUpBot: 'false',
+  pounceNotSureFollowUpBot: 'false',
+  intentFinalReminderBot: 'false',
+  aidOrientationDayOneCaseOneBot: 'false',
+  fafsaFollowUpTwoBot: 'false',
+  orientationBot: 'false',
+  preEnrollmentDayThreeBot: 'false',
+  acceptingLoansThreeBot: 'false',
+  weekBeforeOrientationBot: 'false',
+  housingReminderBot: 'false',
+  fafsaIntroWaveTwoBot: 'false',
+  pounceNotAttendingBot: 'false',
+  pounceNotSureBot: 'false',
+  orientationDoneBot: 'false',
+  aidOrientationDayOneIntroBot: 'false',
+  acceptingLoansTwoBot: 'false',
+  aidOrientationDayTwoBot: 'false',
+  notAttendingSurveyBot: 'false',
+  filterBot: 'false',
+  unknownUserBot: 'false',
+  finAidFollowUpBot: 'false',
+  newStudentOrientationBot: 'false' } // list for wfs
 return new Promise(function(resolve, reject) {
   MongoClient.connect(conf.main, function(err,db) {
     if (err) {
@@ -80,10 +120,21 @@ return new Promise(function(resolve, reject) {
       if (log) {
         studentObj.userExited = "true";
       }
-    }).then(()=> {
+    })
+    .then(()=> {
       return MatcherLogs.find({userId: singleUser.userId, topic: {"$nin": ["/general/failure", "/general/notfound", "/chat/inappropriate"]}}).count();
     }).then((matcherCount) => {
       studentObj.automaticResponses = matcherCount;
+      // compile list of workflows completed
+      let metaKeys = singleUser.meta;
+      _.extend(studentObj, keyList);
+      for (let key in metaKeys) {
+        if (metaKeys[key].started || metaKeys[key].finished) {
+          let wfName = key;
+          keyLog[wfName] = "false";
+          studentObj[wfName] = "true";
+        }
+      }
       statArray.push(studentObj);
       return statArray;
     })
@@ -92,7 +143,7 @@ return new Promise(function(resolve, reject) {
 }).then((array)=> {
   console.log(array);
   const csvStream = csv.createWriteStream({headers: true})
-  const writableStream = fs.createWriteStream("my.csv");
+  const writableStream = fs.createWriteStream("workflowsFinal.csv");
 
   writableStream.on("finish", function() {
     console.log("DONE!");
@@ -101,6 +152,7 @@ return new Promise(function(resolve, reject) {
   array.forEach(function(obj) {
     csvStream.write(obj);
   })
+  console.log("keyLog", keyLog);
   csvStream.end();
 })
 .then(()=> {
